2bec89cc85438e17a58b1306f635e16f25aee15a
KaranDalwaniPerficient <96812384+KaranDalwaniPerficient@users.noreply.github.com>
2022-02-22 10:03:12 +0530

    Add files via upload

--- SASS Assignment/Script/script.js
+++ SASS Assignment/Script/script.js
@@ -53,35 +53,58 @@ $(document).ready(function () {
   $(".info_list li").click(function () {
     $(this).css({ "background-color": "#001352", "color": "white" });
     $(".info_list li").not($(this)).css({ "background-color": "transparent", "color": "#001352" })
+    var name = $(this).text().split(" ")[0].split("(")[0];
+    $("." + name + "").show();
+    $(".info_content").not($("." + name + "")).hide();
   });
+
+  if ($(Window).width() < 426) {
+    $(".info_list li").click(function () {
+      var name = $(this).text().split(" ")[0].split("(")[0];
+      $(".info_content").hide();
+      var clone = $("." + name + "").clone().show();
+      $("." + name + "_cont").show().html(clone).css({ "color": "black" });
+      $("." + name + "_cont ul").css({ "color": "black", "border": "none", });
+      $("." + name + " ul li").css({ "display": "list-item", "list-style": "disc" })
+      $(".cont").not($("." + name + "_cont")).hide().html('');
+      $(".info_list li").css({ "width": "100%", "margin-left": "0" });
+      $(".info_list").css("padding-left", "0");
+    });
+  }
+  $(window).resize(function () {
+    if ($(this).width() < 426 || $(this).width() > 426) {
+      location.reload();
+    }
+  });
+
   $(".info_list li").first().click();
 
-  var main_content_image = [{ image: "Asset/Charging_Point_1-removebg-preview.png", title:"Compatibility" }, { image: "Asset/Charging_Point_2-removebg-preview.png",  title:"The Smart Solution" }, { image: "Asset/Charging_Point_3-removebg-preview.png",  title:"FAQs" },];
+  var main_content_image = [{ image: "Asset/Charging_Point_1-removebg-preview.png", title: "Compatibility" }, { image: "Asset/Charging_Point_2-removebg-preview.png", title: "The Smart Solution" }, { image: "Asset/Charging_Point_3-removebg-preview.png", title: "FAQs" },];
 
   for (i = 0; i < main_content_image.length; i++) {
-    $(".main_content").append('<div class="column center_align font_color"><img src="'+main_content_image[i].image+'" class="object_fit img_size"><h2 class="fnt_sz">'+main_content_image[i].title+'</h2></div>');
+    $(".main_content").append('<div class="column center_align font_color"><div><img src="' + main_content_image[i].image + '" class="object_fit img_size"></div><div><h2 class="fnt_sz">' + main_content_image[i].title + '</h2></div></div>');
   }
   $(".foot_link1").click(function () {
-    $(this).css({ "font-weight": "900"});
-    $(".foot_link1").not($(this)).css({ "font-weight": "500"});
+    $(this).css({ "font-weight": "900" });
+    $(".foot_link1").not($(this)).css({ "font-weight": "500" });
   });
   $(".foot_link1").first().click();
 
   $(".foot_link2").click(function () {
-    $(this).css({ "font-weight": "900"});
-    $(".foot_link2").not($(this)).css({ "font-weight": "500"});
+    $(this).css({ "font-weight": "900" });
+    $(".foot_link2").not($(this)).css({ "font-weight": "500" });
   });
   $(".foot_link2").first().click();
 
   $(".foot_link3").click(function () {
-    $(this).css({ "font-weight": "900"});
-    $(".foot_link3").not($(this)).css({ "font-weight": "500"});
+    $(this).css({ "font-weight": "900" });
+    $(".foot_link3").not($(this)).css({ "font-weight": "500" });
   });
   $(".foot_link3").first().click();
 
   $(".foot_link4").click(function () {
-    $(this).css({ "font-weight": "900"});
-    $(".foot_link4").not($(this)).css({ "font-weight": "500"});
+    $(this).css({ "font-weight": "900" });
+    $(".foot_link4").not($(this)).css({ "font-weight": "500" });
   });
   $(".foot_link4").first().click();
 });
